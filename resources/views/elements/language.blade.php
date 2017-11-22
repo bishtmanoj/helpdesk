1b1cbdf97bd3bb617a7667315183c046361cb8d1
RE User <re84752@norwiga.ispgateway.de>
2017-11-22 20:02:31 +0100

    logo

--- resources/views/elements/language.blade.php
+++ resources/views/elements/language.blade.php
@@ -1,11 +1,11 @@
 <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ config('app.locales')[App::getLocale()] }} <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                @foreach(config('app.locales') as $localeKey =>  $locale)
-                <li><a href="{{ route("locale",[$localeKey]) }}">{{ $locale }}</a></li>
-                @if($localeKey == 'en')
-                <li class="divider"></li>
-                @endif
-                @endforeach
-              </ul>
-            </li>
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">{{ config('app.locales')[\Session::get('locale')] }} <span class="caret"></span></a>
+    <ul class="dropdown-menu" role="menu">
+        @foreach(config('app.locales') as $localeKey =>  $locale)
+        <li><a href="{{ route("locale",[$localeKey]) }}">{{ $locale }}</a></li>
+        @if($localeKey == 'en')
+        <li class="divider"></li>
+        @endif
+        @endforeach
+    </ul>
+</li>
